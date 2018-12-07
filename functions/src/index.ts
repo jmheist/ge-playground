fffb92a13fd6d30157fc3bd1e9921fabcded77c2
Jacob Heisterkamp <jheister@meredith.com>
2018-12-07 16:56:30 -0600

    got email working and started

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,49 @@
-import * as functions from 'firebase-functions';
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// After saving changes, run this in console at root of project: firebase deploy --only functions
+//
+
+
+const functions = require('firebase-functions');
+const sgMail = require('@sendgrid/mail');
+const admin = require('firebase-admin');
+
+//admin.initializeApp(functions.config());
+admin.initializeApp();
+const SENDGRID_API_KEY = functions.config().sendgrid.key;
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+exports.newExchangeCreated = functions.firestore.document("exchanges/{exchangeId}").onCreate(function (snap, context) {
+    const exchange = snap.data();
+    // console.log(exchange);
+    const msg = {
+        to: exchange.adminEmail,
+        from: "jmheist@gmail.com",
+        subject: "New Exchange",
+        templateId: "d-0591b5d5fb0a42a4a05d0135d6774a60",
+        substitutionWrappers: ["{{", "}}"],
+        substitutions: {
+            name: exchange.name
+        }
+    };
+    return sgMail.send(msg);
+});
+
+exports.adminVerifiedEmail = functions.firestore.document("exchanges/{exchangeId}").onUpdate(function (change, context) {
+    const exchangeUpdate = change.after.data();
+    if (!!exchangeUpdate.adminVerifiedEmail) {
+        // console.log(exchange);
+        const msg = {
+            to: exchangeUpdate.adminEmail,
+            from: "jmheist@gmail.com",
+            subject: "exchange has been verified",
+            templateId: "d-0591b5d5fb0a42a4a05d0135d6774a60",
+            substitutionWrappers: ["{{", "}}"],
+            substitutions: {
+                name: exchangeUpdate.name
+            }
+        };
+        return sgMail.send(msg);
+    } else {
+        return;
+    }
+});
